c1e1a5f5dda12487b8f9b985528d5a209a7fcd99
Tayfun Yirdem <1774465+tafo@users.noreply.github.com>
2022-11-12 08:39:23 +0300

    Easy - Add - PailndromeNumber - Solution

---
+++ Easy/PalindromeNumber/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  var number = x.toString();
+  var length = number.length;
+  var loop = length / 2;
+  for (var i = 0; i < loop; i++) {
+    if (number[i] === number[length - 1 - i]) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
--- Easy/TwoSum/Solution.js
+++ Easy/TwoSum/Solution.js
@@ -3,14 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    var hash = {};
-    for(var i = 0; i < nums.length; i++) {
-        var number = nums[i];
-        if(hash[target - number] !== undefined) {
-            return [hash[target - number], i]
-        } else {
-            hash[nums[i]] = i;
-        }
+var twoSum = function (nums, target) {
+  var hash = {};
+  for (var i = 0; i < nums.length; i++) {
+    var number = nums[i];
+    if (hash[target - number] !== undefined) {
+      return [hash[target - number], i];
+    } else {
+      hash[nums[i]] = i;
     }
+  }
 };

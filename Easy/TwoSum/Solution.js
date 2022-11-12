3d4ad6af6526e6bb4d029b769b8d2b543452eaca
Tayfun Yirdem <1774465+tafo@users.noreply.github.com>
2022-11-12 08:35:22 +0300

    Easy - TwoSum - Add - Solution

---
+++ Easy/TwoSum/Solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    var hash = {};
+    for(var i = 0; i < nums.length; i++) {
+        var number = nums[i];
+        if(hash[target - number] !== undefined) {
+            return [hash[target - number], i]
+        } else {
+            hash[nums[i]] = i;
+        }
+    }
+};

3205a03d472252ce3005ad8b9a353e26c36d8e6f
Tayfun Yirdem <1774465+tafo@users.noreply.github.com>
2022-11-13 12:10:09 +0300

    Easy - RemoveDuplicates - Add - Solution

---
+++ Easy/removeDuplicatesFromSortedArray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let previousNumber = nums[0];
+  let currentIndex = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] == previousNumber) continue;
+    nums[currentIndex] = nums[i];
+    previousNumber = nums[i];
+    currentIndex++;
+  }
+  return currentIndex;
+};

8c8b77dde6580640ecee3d553275b31b8ca84038
Tayfun Yirdem <1774465+tafo@users.noreply.github.com>
2022-11-13 12:27:20 +0300

    Easy - RemoveElement - Add - Solution

---
+++ Easy/removeElement.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let currentIndex = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == val) continue;
+    nums[currentIndex] = nums[i];
+    currentIndex++;
+  }
+  return currentIndex;
+};
